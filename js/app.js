a9c6b8610dcc953aef5af958c942e2c6131299d0
Harsimran <harsimransinghbarki@gmail.com>
2018-12-17 19:10:11 +0530

    added html editor and output now updates live

---
+++ js/app.js
@@ -0,0 +1,12 @@
+$('.header__group-buttons').click(function () {
+    $(this).toggleClass('header__group-buttons--active');
+});
+
+$('.main__textarea').height($(window).height() - $('.header').height());
+$('#main-output').width(($(window).width() / 2) - 10);
+
+
+
+$('#main-html').on('change keyup paste', function () {
+    $('.main__iframe').contents().find("html").html($('#main-html').val());
+});
